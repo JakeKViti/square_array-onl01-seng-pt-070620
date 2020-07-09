bc199bfa6dc754d224986714a7333459ced17f93
Jacob Viti <thevitij64@gmail.com>
2020-07-09 23:30:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
   squaredArray = []
-  array.each do { |number| squaredArray << number ** 2 }
+  array.each  { |number| squaredArray << number ** 2 }
   squaredArray
-  end
 end
